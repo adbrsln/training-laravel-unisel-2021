b1abbfb3563757785ac6c89ee4610db733ad834d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2021-01-29 15:48:45 +0800

    Wip user listing

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -49,6 +49,9 @@
                         </div>
 
                     <div class="flex items-center justify-end mt-4">
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('users.index') }}">
+                            {{ __('Cancel') }}
+                        </a>
                         <x-button class="ml-4">
                             {{ __('Add') }}
                         </x-button>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Users...
+                    <div class="flex items-center justify-end mt-4">
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('users.create') }}">
+                            {{ __('Create New User') }}
+                        </a>
+                    </div>
+                    <ol>
+                        @foreach($users as $user)
+                            <li>{{ $user->name }}</li>
+                        @endforeach
+                    </ol>
                 </div>
             </div>
         </div>
