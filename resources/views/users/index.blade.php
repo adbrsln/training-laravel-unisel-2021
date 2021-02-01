31ef27a1a767afaeddd4a940e10cc3779311e63a
Nasrul Hazim <nasrulhazim.m@gmail.com>
2021-02-01 15:18:37 +0800

    Added @can in user.index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,11 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="flex items-center justify-end mb-4">
-                        <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-4" href="{{ route('users.create') }}">
-                            {{ __('Create New User') }}
-                        </a>
-                    </div>
+                    @can('create-user')
+                      <div class="flex items-center justify-end mb-4">
+                          <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-4" href="{{ route('users.create') }}">
+                              {{ __('Create New User') }}
+                          </a>
+                      </div>
+                    @endcan
                     <div class="flex items-center justify-end mt-2 mb-2">
                         {{ $users->links() }}
                     </div>
